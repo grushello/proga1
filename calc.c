6ce9897e512521853992d1db22472ee7a6ea2395
grushello <grushello@gmail.com>
2025-10-17 21:48:58 +0300

    The project is now in stage 'working prototype', Able to calculate all ordinary expressions (no unary). Have to work on locating errors in expressions and need to add parameters to main()

--- calc.c
+++ calc.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
+enum tokenType
+{
+    INVALID, NUMBER, PLUS, MINUS, STAR, SLASH, POW, LPAREN, RPAREN, EOL
+};
+struct Token
+{
+    enum tokenType type;
+    double value;
+    int pos; // 1-based
+};
+
+// DECLARING STUFF__________________________________________________________________________________________________________________________
+char* OUTFILENAME = "out.txt";
+static struct Token* tokens;
+static int tokenCount = 0;
+static int tokenIndex = 0;
+static int errorPos = -1;
+//______________________________________________________________________________________________________________________________________________
 
 char* readFile(const char* filename)
 {
@@ -69,18 +89,192 @@ char* readNextExpression(char** expression, char* currentChar) // writes read da
         return (char*)end + 1;
     return (char*)end;
 }
+void tokenize(const char* expression ) // fills 'tokens'
+{
+    int errorPos = -1;
+    size_t length = strlen(expression);
+    free(tokens);
+    tokens = malloc(length * sizeof(struct Token));
+    size_t i = 0;
+    size_t tlength = 0;
+    while(i < length)
+    {
+        if(isspace(expression[i])) { i++; continue; }
+
+        int startPos = i + 1;
+
+        if(isdigit(expression[i]) || (expression[i] == '.'))
+        {
+            char *endptr;
+            double val = strtod(&expression[i], &endptr);
+            tokens[tlength].type = NUMBER;
+            tokens[tlength].pos = startPos;
+            tokens[tlength].value = val;
+            ++tlength;
+            i = (int)(endptr - expression);
+            continue;
+        }
+        switch (expression[i])
+        {
+        case '+':
+            tokens[tlength++] = (struct Token){PLUS, 0, startPos};
+            ++i;
+            break;
+        case '-':
+            tokens[tlength++] = (struct Token){MINUS, 0, startPos};
+            ++i;
+            break;
+        case '*':
+            if(expression[i+1] == '*')
+            {
+                tokens[tlength++] = (struct Token){POW, 0, startPos}; 
+                i+=2;
+            }
+            else
+            {
+                tokens[tlength++] = (struct Token){STAR, 0, startPos};
+                ++i;
+            }
+            break;
+        case '/':
+            tokens[tlength++] = (struct Token) { SLASH, 0, startPos };
+            ++i;
+            break;
+        case '(' : tokens[tlength++] = (struct Token) { LPAREN, 0, startPos };
+            ++i;
+            break;
+        case ')' : tokens[tlength++] = (struct Token) { RPAREN, 0, startPos };
+            ++i;
+            break;
+        default:
+            tokens[tlength++] = (struct Token) { INVALID, 0, startPos };
+            ++i;
+            break;
+        }
+    }
+    tokens[tlength++] = (struct Token) {EOL, 0, length+1};
+}
+
+double parseExpression();
+double parsePrimary()
+{
+    if(tokens[tokenIndex].type == NUMBER)
+    {
+        return (tokens[tokenIndex++]).value;
+    }
+    else if(tokens[tokenIndex].type == LPAREN)
+    {
+        tokenIndex++;
+        double val = parseExpression();
+        if(tokens[tokenIndex].type == RPAREN)
+        {
+            tokenIndex++;
+            return val;
+        }
+        else
+        {
+            errorPos = tokens[tokenIndex].pos;
+            return 0;
+        }
+    }
+    else
+    {
+        errorPos = tokens[tokenIndex].pos;
+        return 0;
+    }
+}
+double parsePower()
+{
+    double left = parsePrimary();
+    while(tokens[tokenIndex].type == POW)
+    {
+        tokenIndex++;
+        double right = parsePower();
+        left = pow(left, right);
+    }
+    return left;
+}
+double parseTerm()
+{
+    double left = parsePower();
+    {
+        while (tokens[tokenIndex].type == STAR || tokens[tokenIndex].type == SLASH)
+        {
+            struct Token* operation = &tokens[tokenIndex++];
+            double right = parsePower();
+            if(operation->type == SLASH)
+            {
+                if(right == 0.0)
+                {
+                    errorPos = operation->pos;
+                    return 0;
+                }
+                left /= right;
+            }
+            else if(operation->type == STAR)
+            {
+                left *= right;
+            }
+            else
+            {
+                errorPos = operation->pos;
+                return 0;
+            }
+        }
+        return left;
+    }
+}
+double parseExpression()
+{
+    double left = parseTerm();
+    while(tokens[tokenIndex].type == PLUS || tokens[tokenIndex].type == MINUS)
+    {
+        struct Token* operation = &tokens[tokenIndex++];
+        double right = parseTerm();
+        if(operation->type == PLUS)
+        {
+            left += right;
+        }
+        else if (operation->type == MINUS)
+        {
+            left -= right;
+        }
+        else
+        {
+            errorPos = operation->pos;
+            return 0.0;
+        }
+    }
+    return left;
+}
+void calc(const char* expression, FILE* fout)
+{
+    tokenize(expression);
+    tokenIndex = 0;
+    errorPos = -1;
+
+    double result = parseExpression();
+    if(errorPos != -1 || tokens[tokenIndex].type != EOL)
+    {
+        int pos = errorPos != -1? errorPos : tokens[tokenIndex].pos;
+        fprintf(fout, "Error, char: %d\n", pos);\
+        return;
+    }
+    fprintf(fout, "%g\n", result);
+}
 int main(int argc, char** argv)
 {
     char* fileContent = readFile("expr.txt");
     char* currentChar = fileContent;
     char* expression = NULL;
+    FILE* fout = fopen(OUTFILENAME, "w");
     while (*currentChar != '\0')
     {
         currentChar = readNextExpression(&expression, currentChar);
-        printf("%s\n", expression);
+        calc(expression, fout);
         free(expression);
     }
-
+    fclose(fout);
     free(fileContent);
     getchar();
     return 0;
